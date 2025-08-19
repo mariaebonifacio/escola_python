e36b688159513f94fc118ac051c35b99436999a2
mariaebonifacio <madubonifacio12@gmail.com>
2025-08-19 09:59:20 -0300

    tentativa de arrumar o código

--- escola/aluno.py
+++ escola/aluno.py
@@ -14,11 +14,23 @@ def calcular_media(entrada:list[float]) -> float:
 
     # Validando se a entrada é uma lista
     if not isinstance(entrada, list):
-        raise TypeError("Nota inválifa")
+        raise TypeError("Nota inválida")
+    
+     # Validando se a entrada é uma lista
+    if  isinstance(entrada, str):
+        raise ValueError("nota inválida")
+    
 
     # Validando se a lista é vazia
     if len(entrada) == 0:
-        raise ValueError("Não é permitido uma lista vazia")
+        raise ValueError("não é permitido uma lista vazia")
+    
+    for entradas in entrada:
+        if not type(entradas) == int and not type(entradas) == float:
+            raise TypeError("Nota inválida")
+        if entradas < 0 or entradas > 10:
+            raise ValueError("Nota inválida")
         
     media = sum(entrada)/len(entrada)
     return round(media,1)
+
--- tests/test_calcular_media_excecoes.py
+++ tests/test_calcular_media_excecoes.py
@@ -17,3 +17,32 @@ def test_calcular_media_enviando_string():
     with pytest.raises(ValueError, match="não é permitido uma lista com textos"):
         calcular_media(entrada)
 
+def test_calcular_media_enviando_string():
+    #definindo entrada
+    entrada = ["Olá"]
+
+    # Executando a função e esperando erro
+    with pytest.raises( ValueError, match="nota inválida"):
+        calcular_media(entrada)
+
+# --------------------------------------------------------------------------------
+
+# enviando string como nota
+def test_calcular_media_enviando_string_como_nota(notas:list[float]) -> float:
+    # definindo a entrada
+    entrada = [(3.0), 2.0]
+
+    # Executando a função esperando dar erro
+    with pytest.raises(ValueError, match="nota inválida"):
+        calcular_media(entrada)
+
+# --------------------------------------------------------------------------------
+
+def test_media_com_numero_negativo():
+    #definindo a entrada
+    entrada =[-10.0]
+    
+    # executando a função e esperando erro
+    with pytest.raises(ValueError, match="não é permitido uma lista com números negativos"):
+        calcular_media(entrada)
+
